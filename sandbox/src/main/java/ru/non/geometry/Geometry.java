48c530aba2b07f840a30c9aa168a1b5ff6bdff1e
Евгений <wolf495@gmail.com>
2024-02-11 19:37:34 +0300

    Выполнена структуризация кода

---
+++ sandbox/src/main/java/ru/non/geometry/Geometry.java
@@ -0,0 +1,14 @@
+package ru.non.geometry;
+
+import ru.non.geometry.figures.Rectangle;
+import ru.non.geometry.figures.Square;
+
+public class Geometry {
+
+    public static void main(String[] args) {
+        Square.printSquareArea(3);
+        Rectangle.printRectangleArea(3, 5);
+
+    }
+
+}
---
+++ sandbox/src/main/java/ru/non/geometry/figures/Square.java
@@ -0,0 +1,15 @@
+package ru.non.geometry.figures;
+
+public class Square {
+
+    public static void printSquareArea(double side) {
+        String text = String.format("Площадь квадрата со стороной %f = %f " + side, squareArea(side));
+        System.out.println(text);
+    }
+
+    private static double squareArea(double a) {
+        return a * a;
+    }
+
+
+}
