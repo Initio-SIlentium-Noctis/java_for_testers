26f16202b41a4deae8050947eaf8acdd7ec6beee
Евгений <wolf495@gmail.com>
2024-02-29 16:18:05 +0300

    - Реализована двухуровневая архитектура проекта - Добавлен тест для проверки возможности изменения группы - Добавлен класс HelperBase для низкоуровневых методов - Оптимизированы классы LoginHelper и GroupHelper

--- addressbook_web_tests/src/test/java/tests/GroupModificationTests.java
+++ addressbook_web_tests/src/test/java/tests/GroupModificationTests.java
@@ -3,15 +3,13 @@ package tests;
 import model.GroupData;
 import org.junit.jupiter.api.Test;
 
-
-public class GroupRemovalTests extends TestBase {
+public class GroupModificationTests extends TestBase {
 
     @Test
-    public void canRemoveGroup() {
+    public void canModifyGroup() {
         if (!app.groups().isGroupPresent()) {
             app.groups().createGroup(new GroupData("group name", "group header", "group footer"));
         }
-        app.groups().removeGroup();
+        app.groups().modifyGroup(new GroupData().withName("modified name"));
     }
-
 }
