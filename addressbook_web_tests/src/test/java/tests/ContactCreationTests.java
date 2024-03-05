31fb4e04922b83c8887b2aabdc63c5287004296a
Евгений <wolf495@gmail.com>
2024-03-05 18:31:45 +0300

    - Доработаны тесты создания / удаления групп для сравнения списков групп - Изменены методы modifyGroup() и removeGroup() в классе GroupHelper - Удалён избыточный вложенный цикл for в contactProvider

--- addressbook_web_tests/src/test/java/tests/ContactCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/ContactCreationTests.java
@@ -13,17 +13,6 @@ public class ContactCreationTests extends TestBase {
 
     public static List<ContactData> contactProvider() {
         var result = new ArrayList<ContactData>();
-//        for (var firstname : List.of("", randomFirstName())) {
-//            for (var lastname : List.of("", randomLastName())) {
-//                for (var address : List.of("", randomAddress())) {
-//                    for (var email : List.of("", randomEmail())) {
-//                        for (var phone : List.of("", randomPhone())) {
-//                            result.add(new ContactData(firstname, lastname, address, email, phone));
-//                        }
-//                    }
-//                }
-//            }
-//        }
         for (int i = 0; i < 10; i++) {
             result.add(new ContactData(randomFirstName(), randomLastName(), randomAddress(), randomEmail(), randomPhone()));
         }
