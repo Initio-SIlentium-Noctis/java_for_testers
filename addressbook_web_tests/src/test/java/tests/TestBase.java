a7d80ec9c240770796a505d03794d1a77ddeecb2
Евгений <wolf495@gmail.com>
2024-03-12 18:18:45 +0300

    - добавлен конфигурационный файл local.properties содержащий настройки окружения - изменены классы TestBase и ApplicationManager для чтения данных из файла настроек

--- addressbook_web_tests/src/test/java/tests/TestBase.java
+++ addressbook_web_tests/src/test/java/tests/TestBase.java
@@ -3,6 +3,10 @@ package tests;
 import manager.ApplicationManager;
 import org.junit.jupiter.api.BeforeEach;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.Random;
 
 public class TestBase {
@@ -10,10 +14,12 @@ public class TestBase {
     protected static ApplicationManager app;
 
     @BeforeEach
-    public void setUp() {
+    public void setUp() throws IOException {
         if (app == null) {
+            var properties = new Properties();
+            properties.load(new FileReader(System.getProperty("target", "local.properties")));
             app = new ApplicationManager();
-            app.init(System.getProperty("browser", "firefox"));
+            app.init(System.getProperty("browser", "firefox"), properties);
         }
     }
 
