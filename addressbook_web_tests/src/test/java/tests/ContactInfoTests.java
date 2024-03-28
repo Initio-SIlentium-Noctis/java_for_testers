7c189c3dacaf401e13ac5606d4ec101bacb51a04
Евгений <wolf495@gmail.com>
2024-03-28 19:01:37 +0300

    - Переделаны тесты проверки данных контактов. Теперь вместо трёх тестов все дополнительные данные контактов (адрес, почто, телефоны) проверяются в одном тесте

--- addressbook_web_tests/src/test/java/tests/ContactInfoTests.java
+++ addressbook_web_tests/src/test/java/tests/ContactInfoTests.java
@@ -10,47 +10,31 @@ import java.util.stream.Stream;
 public class ContactInfoTests extends TestBase {
 
     @Test
-    void testPhones() {
+    void testContactInfo() {
         if (app.contacts().getCount() == 0) {
             app.contacts().createContact(new ContactData("", "Пётр", "Иванов", "Г.Москва, ул. Мира 6, кв.25", "ivanov24@gmail.com", "", "","src/test/resources/images/avatar.png", "", "+79262545574", "", ""));
         }
         var contacts = app.hbm().getContactList();
-        var expected = contacts.stream().collect(Collectors.toMap(contact -> contact.id(), contact ->
-            Stream.of(contact.home(), contact.mobile(), contact.work(), contact.secondary())
-                    .filter(s -> s != null && !s.equals(""))
-                    .collect(Collectors.joining("\n"))
-        ));
         var phones = app.contacts().getPhones();
-        Assertions.assertEquals(expected, phones);
-    }
-
-    @Test
-    void testAddress() {
-        if (app.contacts().getCount() == 0) {
-            app.contacts().createContact(new ContactData("", "Пётр", "Иванов", "Г.Москва, ул. Мира 6, кв.25", "ivanov24@gmail.com", "", "","src/test/resources/images/avatar.png", "", "+79262545574", "", ""));
-        }
-        var contacts = app.hbm().getContactList();
+        var address = app.contacts().getAddress();
+        var emails = app.contacts().getEmails();
         var expected = contacts.stream().collect(Collectors.toMap(contact -> contact.id(), contact ->
+                Stream.of(contact.home(), contact.mobile(), contact.work(), contact.secondary())
+                        .filter(s -> s != null && !s.equals(""))
+                        .collect(Collectors.joining("\n"))
+        ));
+        Assertions.assertEquals(expected, phones);
+        expected = contacts.stream().collect(Collectors.toMap(contact -> contact.id(), contact ->
                 Stream.of(contact.address())
                         .filter(s -> s != null && !s.equals(""))
                         .collect(Collectors.joining("\n"))
         ));
-        var address = app.contacts().getAddress();
         Assertions.assertEquals(expected, address);
-    }
-
-    @Test
-    void testEmails() {
-        if (app.contacts().getCount() == 0) {
-            app.contacts().createContact(new ContactData("", "Пётр", "Иванов", "Г.Москва, ул. Мира 6, кв.25", "ivanov24@gmail.com", "", "", "src/test/resources/images/avatar.png", "", "+79262545574", "", ""));
-        }
-        var contacts = app.hbm().getContactList();
-        var expected = contacts.stream().collect(Collectors.toMap(contact -> contact.id(), contact ->
+        expected = contacts.stream().collect(Collectors.toMap(contact -> contact.id(), contact ->
                 Stream.of(contact.email(), contact.email2(), contact.email3())
                         .filter(s -> s != null && !s.equals(""))
                         .collect(Collectors.joining("\n"))
         ));
-        var emails = app.contacts().getEmails();
         Assertions.assertEquals(expected, emails);
     }
 
