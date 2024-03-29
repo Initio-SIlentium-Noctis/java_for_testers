4e0d128343ce72617614ef2cebcc66399b4666df
Евгений <wolf495@gmail.com>
2024-03-29 15:45:51 +0300

    - Добавлен модуль mantis_tests для тестов приложения mantis - Создан базовые классы ApplicationManager, SessionHelper TestBase и HelperBase - Добавлен первый тест для проверки авторизации в приложении mantis

---
+++ mantis_tests/src/test/java/ru/Non/mantis/tests/LoginTests.java
@@ -0,0 +1,16 @@
+package ru.Non.mantis.tests;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class LoginTests extends TestBase {
+
+    @Test
+    void canLogin() {
+        app.session().login("administrator", "root");
+        Assertions.assertTrue(app.session().isLoggedIn());
+    }
+
+
+
+}
